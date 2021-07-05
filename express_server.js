6cba12cdc70719c6eb8d3767fdcbee6a2c87b174
Yves Candau <ycandau@gmail.com>
2021-07-05 23:24:23 +0000

    Added ejs partial for shared header

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@
 //------------------------------------------------------------------------------
 // Require modules
 
-const express = require("express");
+const express = require('express');
 
 //------------------------------------------------------------------------------
 // Set constants
@@ -12,40 +12,40 @@ const express = require("express");
 const PORT = 8080;
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b2xVn2: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
 };
 
 //------------------------------------------------------------------------------
 // Create and initialize server
 
 const app = express();
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //------------------------------------------------------------------------------
 // Set endpoints
 
-app.get("/", (req, res) => {
-  res.send("Hello!\n");
+app.get('/', (req, res) => {
+  res.send('Hello!\n');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 //------------------------------------------------------------------------------
