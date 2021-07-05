350372a1b9f6617a6d9524d5b7ad252a530f9fba
Yves Candau <ycandau@gmail.com>
2021-07-05 21:18:13 +0000

    Added endpoint to access url database object as a JSON string

--- express_server.js
+++ express_server.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 //------------------------------------------------------------------------------
 // Start listening
 
